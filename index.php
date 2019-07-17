941be0322f349e698abb7ebb945329ccf8d991b6
root <root@RingoverWEB-154.44.180.76>
2019-07-17 11:09:05 +0200

    ringover version

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no" />
-<title>HTML5 Speedtest</title>
+<title>Ringover Speedtest</title>
 <style type="text/css">
 	html,body{
 		border:none; padding:0; margin:0;
@@ -16,38 +16,12 @@
 	}
 	h1{
 		color:#404040;
-	}
-	#startStopBtn{
-		display:inline-block;
-		margin:0 auto;
-		color:#6060AA;
-		background-color:rgba(0,0,0,0);
-		border:0.15em solid #6060FF;
-		border-radius:0.3em;
-		transition:all 0.3s;
-		box-sizing:border-box;
-		width:8em; height:3em;
-		line-height:2.7em;
-		cursor:pointer;
-		box-shadow: 0 0 0 rgba(0,0,0,0.1), inset 0 0 0 rgba(0,0,0,0.1);
-	}
-	#startStopBtn:hover{
-		box-shadow: 0 0 2em rgba(0,0,0,0.1), inset 0 0 1em rgba(0,0,0,0.1);
-	}
-	#startStopBtn.running{
-		background-color:#FF3030;
-		border-color:#FF6060;
-		color:#FFFFFF;
-	}
-	#startStopBtn:before{
-		content:"Start";
-	}
-	#startStopBtn.running:before{
-		content:"Abort";
+		margin-top:50px;
 	}
 	#test{
 		margin-top:2em;
 		margin-bottom:12em;
+		margin-top:120px;;	
 	}
 	div.testArea{
 		display:inline-block;
@@ -87,12 +61,24 @@
 	div.testGroup{
 		display:inline-block;
 	}
+	#logo{
+		margin-top:80px;
+	}
+	#ipArea{
+			margin-top:100px;
+	}
 	@media all and (max-width:65em){
 		body{
 			font-size:1.5vw;
 		}
 	}
 	@media all and (max-width:40em){
+		div.meterText{
+			font-size:2em;
+		}
+		div.testName{
+			font-size:1em;
+		}
 		body{
 			font-size:0.8em;
 		}
@@ -100,16 +86,32 @@
 			display:block;
 			margin: 0 auto;
 		}
+		#test{
+			margin-top:20px;	
+		}
+	    div.testArea{
+			width: 13em;
+    		height: 11em;
+			margin-top:20px;
+			margin-bottom:20px;
+		}
+		#ipArea{
+			margin-top:20px;
+		}
+		#logo{
+			margin-top:20px;
+			margin-bottom:20px;
+		}
 	}
 
 </style>
 <script type="text/javascript">
 function I(id){return document.getElementById(id);}
 var meterBk="#E0E0E0";
-var dlColor="#6060AA",
-	ulColor="#309030",
-	pingColor="#AA6060",
-	jitColor="#AA6060";
+var dlColor="#3498db",
+	ulColor="#5fd1d3",
+	pingColor="#9b59b6",
+	jitColor="#f1c40f";
 var progColor="#EEEEEE";
 
 //CODE FOR GAUGES
@@ -155,19 +157,16 @@ function startStop(){
 		w.postMessage('abort');
 		w=null;
 		data=null;
-		I("startStopBtn").className="";
 		initUI();
 	}else{
 		//test is not running, begin
 		w=new Worker('speedtest_worker.min.js');
 		w.postMessage('start'); //Add optional parameters as a JSON object to this command
-		I("startStopBtn").className="running";
 		w.onmessage=function(e){
 			data=JSON.parse(e.data);
 			var status=data.testState;
 			if(status>=4){
 				//test completed
-				I("startStopBtn").className="";
 				w=null;
 				updateUI(true);
 			}
@@ -213,13 +212,13 @@ function initUI(){
 	I("pingText").textContent="";
 	I("jitText").textContent="";
 	I("ip").textContent="";
+	startStop();
 }
 
 </script>
 </head>
 <body>
-<h1>HTML5 Speedtest</h1>
-<div id="startStopBtn" onclick="startStop()"></div>
+<img id="logo" src="https://myringover.com/img/logo-ringover-full.svg" /><br />
 <div id="test">
 	<div class="testGroup">
 		<div class="testArea">
@@ -252,8 +251,8 @@ function initUI(){
 	<div id="ipArea">
 		IP Address: <span id="ip"></span>
 	</div>
+<!--div id="startStopBtn" onclick="startStop()"></div-->
 </div>
-<a href="https://github.com/adolfintel/speedtest">Source code</a>
 <script type="text/javascript">setTimeout(initUI,100);</script>
 </body>
 </html>
